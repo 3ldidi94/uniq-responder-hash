b95c797c6aa997c5cb75a67dc271309424c32cd3
3ldidi <elie@laracaille.fr>
2023-02-05 02:44:02 +0100

    Add filter for users and for delete machine accounts to the output

--- unik-responder-hash.py
+++ unik-responder-hash.py
@@ -8,34 +8,51 @@ import argparse
 import sys
 from os.path import exists
 
-def reader(file, ofile="unik-output.txt"):
+def reader(file, ofile, machine_hash, print_users):
     users = []
     final_hash = []
     nbr_hash = 0
+
     with open(file, 'r') as data:
         data = data.read().splitlines()
         for hash in data:
             user = hash.split('::')[0]
             if user not in users:
-                users.append(user)
-                final_hash.append(hash)
+                if machine_hash:
+                    users.append(user)
+                    final_hash.append(hash)
+                else:
+                    if not "$" in user[-1]:
+                        users.append(user)
+                        final_hash.append(hash)
     
     with open(ofile, 'w') as data:
         for hash in final_hash:
             nbr_hash += 1
             data.write(hash+'\n')
-    print (f"{nbr_hash} uniq hashes written to {ofile} !")
+        if print_users:
+            #for user in users:
+            #    print (f"{user}")
+            print (f"Users :\n{users}")
+    print (f"\n[+] {nbr_hash} uniq hashes written to {ofile} !")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="A program that remove several hash for a same user. It is a Responder log parser.")
+    parser = argparse.ArgumentParser(description="A program that remove several hashes for a same user. For Responder NetNTLMv2 log file.")
     parser.add_argument("-f","--file", help="File of responder hash to parse.")
-    parser.add_argument("-o","--output", help="Output file.")
- 
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit(1)
+    parser.add_argument("-u","--users", default=False, action="store_true", help="Print users parsed from the specified file.")
+    parser.add_argument("-m","--machine", default=False, action="store_true", help="Keep the machines NetNTLMv2 hash in the output file. Default these hashes are deleted.")
+    parser.add_argument("-o","--output", default="unik-output.txt", help="Output file. Default : unik-output.txt")
     
     args = parser.parse_args()
 
     if args.file:
-        reader(args.file, args.output)
+        try:
+            if exists(args.file):
+                reader(args.file, args.output, args.machine, args.users)
+            else:
+                sys.exit(f"The specified file '{args.file}' was not found.")
+        except Exception as error:
+            sys.exit(f"ERROR :\nException : {error}")
+
+    else:
+        sys.exit("[!] You must specified a NetNTLMv2 file ! Try '-h' for the help.")
